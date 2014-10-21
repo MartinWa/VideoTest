ec01c84ebbaa7cf5d3c12c78ec7ad8ab26711296
Martin Wåger <martin.w@comaround.se>
2014-10-21 16:08:54 +0200

    shortsas does not work

--- VideoTest/Infrastructure/Constants.cs
+++ VideoTest/Infrastructure/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        private const string Key = "xxx";
+        private const string Key = "";
         public const string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=zerodev;AccountKey=" + Key;
         public const string ContainerName = "portal-201/guides/266172/";
         public const string FileName = "The Amazing Spiderman bionic.mp4";
